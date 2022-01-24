71d9e6309721e71b944485f33bb52e01827e21d8
SathyaNarayan <roflmeatu@gmail.com>
2022-01-24 23:41:55 +0100

    Segmented View

--- SegmentedControl/ViewController.swift
+++ SegmentedControl/ViewController.swift
@@ -8,12 +8,32 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var mySegment: UISegmentedControl!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func segmentChanged(_ sender: Any) {
+        
+        
+        
+        switch mySegment.selectedSegmentIndex {
+        case 0:
+            print("Male")
+        case 1:
+            print("Female")
+        case 2:
+            print("Others")
+        default:
+            break
+        }
+        
+        
+        
+    }
+    
 }
 
